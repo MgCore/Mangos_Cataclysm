5e4e58c37181fc1369e1f0bba2625e91d0497e3b
LordJZ <jouterr@gmail.com>
2011-02-18 23:21:38 +0300

    Fix CMSG_CAST_SPELL, hide log files

--- src/game/SpellHandler.cpp
+++ src/game/SpellHandler.cpp
@@ -323,6 +323,7 @@ void WorldSession::HandleCastSpellOpcode(WorldPacket& recvPacket)
     uint8  cast_count, unk_flags;
     recvPacket >> cast_count;
     recvPacket >> spellId;
+    recvPacket.read_skip<uint32>();                         // unknown id
     recvPacket >> unk_flags;                                // flags (if 0x02 - some additional data are received)
 
     // ignore for remote control state (for player case)
